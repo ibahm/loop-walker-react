7c25ab39476ae9e5fcc46d7381d40f89af99d464
Ibahm <ibrahimahm92@gmail.com>
2022-09-16 14:52:51 +0100

    Initial board state
    - useState and init function to initialise the state
    - map function to render and pass props through children

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,28 +1,50 @@
 import React, {useState} from "react";
 import Cell from "./Cell";
 
-const Grid = () => { 
+/*
+    type = 'cell', 'cell-start', 'cell-curr', 'cell-end'
+    row = i index
+    col = j index
+*/
 
-    const [board, setBoard] = useState([]);
-
-    /*
-        type = 'cell', 'cell-start', 'cell-curr', 'cell-end'
-        row = i index
-        col = j index
-    */
-
-    class Spot{
-        constructor(type, row, col) {
-            this.type = type;
-            this.row = row;
-            this.col = col;
-        }
+class Spot{
+    constructor(type, row, col) {
+        this.type = type;
+        this.row = row;
+        this.col = col;
     }
+}
+
+// Initial board state on render
+
+function initBoard() {
+    let n = 32;
+    let arr = new Array(n)
+
+    for(let i = 0; i < n; i++) {
+        arr[i] = new Array(n);
+    }
+    
+    for(let i = 0; i < n; i++) {
+       for(let j = 0; j < n; j++) {
+        arr[i][j] = new Spot("cell", i, j);
+       }
+    }
+
+    return arr;
+}
+
+const Grid = () => { 
+    const [board, setBoard] = useState(() => initBoard());
 
     return (
         <div className="grid-cont">
             <div className ="grid">
-                <Cell/>
+                {board.map((spot) => (
+                    board.map((spot, index) => (
+                        <Cell type={spot[index].type} row={spot[index].row} col={spot[index].col}/>
+                    ))
+                ))}
             </div>
             <button>START</button>
         </div>               
