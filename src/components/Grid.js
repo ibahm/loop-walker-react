eef26bd38bb4dcba820b2d8815f3db354b20a860
Ibahm <ibrahimahm92@gmail.com>
2022-09-25 13:46:45 +0100

    Pathing function & re-rendering DOM

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -2,15 +2,12 @@ import React, {useState} from "react";
 import Cell from "./Cell";
 import {nanoid} from "nanoid"
 
-/*
-    Global variables
-    Update css grid when changing BOARD_SIZE
-*/
+// Global variables
 
 const BOARD_SIZE = 64;
 
 /*
-    type = 'cell', 'cell-start', 'cell-curr', 'cell-end'
+    type = 'cell', 'cell-start', 'cell-current', 'cell-end'
     row = i index
     col = j index
 */
@@ -44,6 +41,49 @@ function initBoard() {
 const Grid = () => { 
     const [board, setBoard] = useState(() => initBoard());
 
+    function beginPath() {
+        let path = new Array();
+        let curr = board[BOARD_SIZE/2][BOARD_SIZE/2];
+        let prev = null;
+
+        for(let i = 0; i < 1000; i++) {
+            path.push(Math.floor(Math.random(0)*4))
+        }
+
+        // Every 0.2 seconds => i++
+
+        let i = 0;
+        let interval = setInterval(() => {
+            switch(path[i]) {
+                case 0:
+                    prev = curr
+                    curr = board[curr.row-1][curr.col]
+                    break;
+                case 1:
+                    prev = curr
+                    curr = board[curr.row+1][curr.col]
+                    break;
+                case 2:
+                    prev = curr
+                    curr = board[curr.row][curr.col+1]
+                    break;
+                case 3:
+                    prev = curr
+                    curr = board[curr.row][curr.col-1]
+                    break;
+            }
+            curr.type="cell-current"
+            prev.type="cell-end"
+            setBoard([...board])
+
+            console.log(path[i++]);
+            
+            if(i === path.length){
+                clearInterval(interval);
+            }
+        }, 200)
+    }
+
     // Always render the most middle cell as green
 
     if (board[BOARD_SIZE/2][BOARD_SIZE/2].type !== "cell-start") {
@@ -52,16 +92,18 @@ const Grid = () => {
     }
 
     return (
-        <div className="grid-cont">
-            <div className ="grid">
-                {board.map((spot, index) => (
-                    board.map(spot => (
-                        <Cell key={nanoid()} type={spot[index].type} row={spot[index].row} col={spot[index].col} board={board}/>
-                    ))                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
-                ))}
+        <>
+            <div className="grid-cont">
+                <div className ="grid">
+                    {board.map((spot, index) => (
+                        board.map(spot => (
+                            <Cell key={nanoid()} type={spot[index].type} row={spot[index].row} col={spot[index].col} board={board}/>
+                        ))                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+                    ))}
+                </div>
+                <button className="start" onClick={beginPath}>START</button>
             </div>
-            <button className="start">START</button>
-        </div>               
+        </>                   
     )
 }
 
