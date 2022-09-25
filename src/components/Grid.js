598b6a07c0ac22917fc9ea674fda55cd51030826
Ibahm <ibrahimahm92@gmail.com>
2022-09-25 14:30:44 +0100

    Update cell in intervals

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -5,6 +5,9 @@ import {nanoid} from "nanoid"
 // Global variables
 
 const BOARD_SIZE = 64;
+const PATH_SPEED = 200;
+let CURR_CELL = null;
+let PREV_CELL = null;
 
 /*
     type = 'cell', 'cell-start', 'cell-current', 'cell-end'
@@ -38,50 +41,57 @@ function initBoard() {
     return arr;
 }
 
+// Update visited cell
+
+function updateCell(array, i, path) {
+    switch(path[i]) {
+        case 0:
+            PREV_CELL = CURR_CELL
+            CURR_CELL = array[CURR_CELL.row-1][CURR_CELL.col]
+            break;
+        case 1:
+            PREV_CELL = CURR_CELL
+            CURR_CELL = array[CURR_CELL.row+1][CURR_CELL.col]
+            break;
+        case 2:
+            PREV_CELL = CURR_CELL
+            CURR_CELL = array[CURR_CELL.row][CURR_CELL.col+1]
+            break;
+        case 3:
+            PREV_CELL = CURR_CELL
+            CURR_CELL = array[CURR_CELL.row][CURR_CELL.col-1]
+            break;
+    }
+    CURR_CELL.type="cell-current"
+    PREV_CELL.type="cell-end"
+
+    return array;
+}
+
 const Grid = () => { 
     const [board, setBoard] = useState(() => initBoard());
 
     function beginPath() {
         let path = new Array();
-        let curr = board[BOARD_SIZE/2][BOARD_SIZE/2];
-        let prev = null;
+        CURR_CELL = board[BOARD_SIZE/2][BOARD_SIZE/2]
 
         for(let i = 0; i < 1000; i++) {
             path.push(Math.floor(Math.random(0)*4))
         }
 
-        // Every 0.2 seconds => i++
-
+        // Call function in interval
+        
         let i = 0;
         let interval = setInterval(() => {
-            switch(path[i]) {
-                case 0:
-                    prev = curr
-                    curr = board[curr.row-1][curr.col]
-                    break;
-                case 1:
-                    prev = curr
-                    curr = board[curr.row+1][curr.col]
-                    break;
-                case 2:
-                    prev = curr
-                    curr = board[curr.row][curr.col+1]
-                    break;
-                case 3:
-                    prev = curr
-                    curr = board[curr.row][curr.col-1]
-                    break;
-            }
-            curr.type="cell-current"
-            prev.type="cell-end"
-            setBoard([...board])
+            
+            setBoard([...updateCell(board, i, path)])
 
             console.log(path[i++]);
             
             if(i === path.length){
                 clearInterval(interval);
             }
-        }, 200)
+        }, PATH_SPEED)
     }
 
     // Always render the most middle cell as green
