aa9285b7e2be934be007c299813256753d58d6e1
Ibahm <ibrahimahm92@gmail.com>
2022-10-03 19:04:13 +0100

    Updated guide & styling changes
    - Added colours to maintain a theme
    - Updated guide for more useful information

--- src/components/Guide.js
+++ src/components/Guide.js
@@ -10,13 +10,20 @@ const Guide = () => {
             </div>
             <span>
                 Before pressing start, make sure that the 'Array of directions' in logs is displaying an array.
-                If <b>no data is found</b>, then <b>click the 'New Path' button</b> to generate a valid array.
+                If <b style={{color: "rgb(136, 128, 253)"}}>no data is found</b>, then <b style={{color: "rgb(136, 128, 253)"}}>click the 'New Path' button</b> to generate a valid array.
             </span>
             <span>
                 Always wait for an array of directions to be generated before pressing start.    
             </span>
             <span>
-                Enjoy 😄.
+                The <b style={{color: "rgb(136, 128, 253)"}}>array of directions</b> is what the algorithm reads. 
+                It finds the next cell to visit based on the element at the nth index.
+            </span>
+            <span>
+                The <b style={{color: "rgb(136, 128, 253)"}}>route</b> is the relative direction the algorithm is moving to.
+            </span>
+            <span>
+            <b style={{color: "rgb(136, 128, 253)"}}>Enjoy 😄</b>
             </span> 
             <div className="side-footer">
                 <a href="https://github.com/ibahm" target="_blank" rel="noopener noreferrer">
--- src/components/Logs.js
+++ src/components/Logs.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 
 const Logs = ({count, visited, path}) => {
     return (
@@ -7,9 +7,9 @@ const Logs = ({count, visited, path}) => {
                 <h1>Logs</h1>
                 <span className="emoji" role="img" aria-label="open-file-folder">📂</span>
             </div>
-            <span className="logs-steps"><b>Steps:</b> {count}</span>
-            <span className="logs-results"><b>Array of directions:</b> {path.length > 0 ? `{${path}}` : "No data found..."}</span>
-            <span className="logs-visited"><b>Route:</b> {visited.length > 0 ? `{${visited}}` : "No data found..."}</span>
+            <span className="logs-steps"><b style={{color: "rgb(136, 128, 253)"}}>Steps:</b> {count}</span>
+            <span className="logs-results"><b style={{color: "rgb(136, 128, 253)"}}>Array of directions:</b> {path.length > 0 ? `{${path}}` : "No data found..."}</span>
+            <span className="logs-visited"><b style={{color: "rgb(136, 128, 253)"}}>Route:</b> {visited.length > 0 ? `{${visited}}` : "No data found..."}</span>
         </div>
     )
 }
