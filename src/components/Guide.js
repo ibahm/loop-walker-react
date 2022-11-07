f845b0efa0cf63a4316f7c91f2c5820535ea44b9
Ibahm <ibrahimahm92@gmail.com>
2022-11-07 12:56:42 +0000

    Removed redundant information

--- src/components/Guide.js
+++ src/components/Guide.js
@@ -10,17 +10,9 @@ const Guide = () => {
             </div>
             <span>
                 <b style={{color: "rgb(136, 128, 253)"}}>Click the 'New Path' button</b> to generate directions.
-                Wait for the algorithm to generate an array.
-            </span>
-            <span>
-                The <b style={{color: "rgb(136, 128, 253)"}}>generated directions</b> is what the algorithm interprets as a route. 
-                It finds the next cell to visit based on the element at the nth index. Think of it as a list of directions to travel.
             </span>
             <span>
                 The <b style={{color: "rgb(136, 128, 253)"}}>route</b> is a readable version of directions.
-            </span>
-            <span>
-            <b style={{color: "rgb(136, 128, 253)"}}>0 = left, 1 = right, 2 = up, 3 = down</b>
             </span>
             <span>
             <b style={{color: "rgb(136, 128, 253)"}}>Enjoy 😄</b>
